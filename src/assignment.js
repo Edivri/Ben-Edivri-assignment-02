8c803cab141e748783a9baf68b32c29462bf8d3d
Ben Edivri <47800819+Edivri@users.noreply.github.com>
2022-07-14 22:27:52 +0000

    attempted 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,8 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum=0;
+  for (let index = 0; index < arrayOfNumbers; index++) {
+    sum += arrayOfNumbers[i];
+  }
+    return sum.;
 }
+assignment.sumOfNumbers = sumOfNumbers;
 
 
 /**
@@ -28,9 +33,10 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    const even = arrayOfNumbers.filter(num => num%2);
+    return even.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
